3d8eb1e2d692e1289e1a7aedd23f7c3a342a3a16
dziky <62235247+dziky93@users.noreply.github.com>
2022-11-30 22:16:40 +0700

    Add files via upload

---
+++ Array2dimensi.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package array2dimensi;
+
+/**
+ *
+ * @author DzikyR
+ */
+public class Array2dimensi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("---------------------------------------");
+        System.out.println("********* ARRAY MULTIDIMENSI**********");
+        System.out.println("---------------------------------------");
+        //System.out.println("\n");
+             
+        
+        
+        int Array_1[][] = {{10,20,30},{1,2,3},{40,50,60}};
+        System.out.println("array baris 0 kolom 0 adalah "+Array_1[0][0]);
+        System.out.println("array baris 0 kolom 1 adalah "+Array_1[0][1]);
+        System.out.println("array baris 0 kolom 2 adalah "+Array_1[0][2]);
+        System.out.println("array baris 1 kolom 0 adalah "+Array_1[1][0]);
+        System.out.println("array baris 1 kolom 1 adalah "+Array_1[1][1]);
+        System.out.println("array baris 1 kolom 2 adalah "+Array_1[1][2]);
+        System.out.println("array baris 2 kolom 0 adalah "+Array_1[2][0]);
+        System.out.println("array baris 2 kolom 1 adalah "+Array_1[2][1]);
+        System.out.println("array baris 2 kolom 2 adalah "+Array_1[2][2]);
+        
+        System.out.println("-------------------------------------------");
+        
+        String Array_2[][] = {{"41","c2","72","e8"},{"56","12","7","28"},{"bf","31","11","6b"}};
+        System.out.println("array baris 0 kolom 0 adalah "+Array_2[0][0]);
+        System.out.println("array baris 0 kolom 1 adalah "+Array_2[0][1]);
+        System.out.println("array baris 0 kolom 2 adalah "+Array_2[0][2]);
+        System.out.println("array baris 0 kolom 3 adalah "+Array_2[0][3]);
+        System.out.println("array baris 1 kolom 0 adalah "+Array_2[1][0]);
+        System.out.println("array baris 1 kolom 1 adalah "+Array_2[1][1]);
+        System.out.println("array baris 1 kolom 2 adalah "+Array_2[1][2]);
+        System.out.println("array baris 1 kolom 3 adalah "+Array_2[1][3]);
+        System.out.println("array baris 2 kolom 0 adalah "+Array_2[2][0]);
+        System.out.println("array baris 2 kolom 1 adalah "+Array_2[2][1]);
+        System.out.println("array baris 2 kolom 2 adalah "+Array_2[2][2]);
+        System.out.println("array baris 2 kolom 3 adalah "+Array_2[2][3]);
+        
+        
+        //String Array_3[][] = {{"Wicak","Saiful","Hanafi"},{"Ina","Rere","Nidia"},{"Nia","Fajar","Lilik"}};
+        //System.out.println("Array baris 2 dan kolom ke 1 adalah "+Array_3[2][1]);
+                
+    }
+                
+    
+}
